c0a077832566c677f1168ac9fdb858bd4ce707fe
anonymoussc <50c5ac69@opayq.com>
2020-08-03 03:16:34 +0700

    Initial commit

---
+++ src/test/java/com/enoct/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enoct.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
